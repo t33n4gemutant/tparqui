d4622f90daa6a5a3dc087bf93bd2e032562afc07
Federelli <federelli@gmail.com>
2012-06-12 02:38:04 -0300

    Initial
    :0

---
+++ src/kernel.c
@@ -0,0 +1,11 @@
+#include "../kernel/driver/monitor.h"
+
+/**********************************************
+ kmain()
+ Punto de entrada de código C.
+ *************************************************/
+
+kmain() {
+	monitor_clear();
+	monitor_write("Hello, world!");
+}
