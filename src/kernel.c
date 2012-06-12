4e672adf2a9b8c8c96a34b534a04f271baee4e22
Federelli <federelli@gmail.com>
2012-06-12 12:49:27 -0300

    Basic interrupt handlers

--- src/kernel.c
+++ src/kernel.c
@@ -1,11 +1,18 @@
 #include "kernel/driver/monitor.h"
-
+#include "kernel/system/descriptor_tables.h"
 /**********************************************
  kmain()
  Punto de entrada de código C.
  *************************************************/
 
 kmain() {
+	// Initialize all the ISRs and segmentation
+	init_descriptor_tables();
+	// Initialize the screen (by clearing it)
 	monitor_clear();
-	monitor_write("Hello, world!");
+	// Write out a sample string
+	monitor_write("Hello, world!\n");
+
+	asm volatile("int $0x3");
+	asm volatile("int $0x4");
 }
